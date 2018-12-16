4e11d00568c2bb9c55a4f147f6a2806f3ffac2f4
Chris Hulbert <chris.hulbert@gmail.com>
2018-12-17 09:25:01 +1100

    Extract all

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,18 @@ fn u32_to_u8_slice(original: &[u32]) -> &[u8] {
     return unsafe { slice::from_raw_parts(ptr, count) };
 }
 
-fn main() -> io::Result<()> {
-    let raw: Vec<u8> = fs::read("data/VGAGR0.DAT")?;
-    let data = decompressor::decompress(&raw)?;
+fn extract(index: u8) -> io::Result<()> {
+    println!("Extracting {}", index);
+    fs::create_dir_all(format!("output/{}", index));
+
+    let data_file: Vec<u8> = fs::read(format!("data/VGAGR{}.DAT", index))?;
+    let data = decompressor::decompress(&data_file)?;
     println!("Number of sections: {:?}", data.len());
     for s in 0..data.len() {
         println!("Section #{}: {} bytes", s, data[s].len());
     }
 
-    let ground_file: Vec<u8> = fs::read("data/GROUND0O.DAT")?;
+    let ground_file: Vec<u8> = fs::read(format!("data/GROUND{}O.DAT", index))?;
     let ground = ground::parse(&ground_file)?;
     println!("VGA: {:x}", ground.palettes.vga_custom[0]);
 
@@ -35,7 +38,7 @@ fn main() -> io::Result<()> {
             let sprite = sprites::extract(&data[0], terrain.width, terrain.height, terrain.image_loc, terrain.mask_loc, &palette);
             println!("{}: {:?}", i, terrain);
 
-            let file = format!("terrain_{}.png", i);
+            let file = format!("output/{}/terrain_{}.png", index, i);
             let buf = u32_to_u8_slice(&sprite);
             image::save_buffer(file, buf, terrain.width as u32, terrain.height as u32, image::RGBA(8)).unwrap();
         }
@@ -46,10 +49,17 @@ fn main() -> io::Result<()> {
             let sprite = sprites::extract(&data[1], object.width, object.height, object.animation_frames_base_loc, object.animation_frames_base_loc + object.mask_offset_from_image, &palette);
             println!("{}: {:?}", i, object);
 
-            let file = format!("object_{}.png", i);
+            let file = format!("output/{}/object_{}.png", index, i);
             let buf = u32_to_u8_slice(&sprite);
             image::save_buffer(file, buf, object.width as u32, object.height as u32, image::RGBA(8)).unwrap();
         }
+    }
+        Ok(())
+}
+
+fn main() -> io::Result<()> {
+    for i in 0..5 {
+        extract(i)?;
     }
 
     Ok(())
