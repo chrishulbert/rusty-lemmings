4ab9a7361795dadc9c6b3902f606f836be82fe62
Chris Hulbert <chris.hulbert@gmail.com>
2022-08-03 17:47:56 +1000

    Dig

--- src/lemmings_to_bevy/load_lemmings_textures.rs
+++ src/lemmings_to_bevy/load_lemmings_textures.rs
@@ -1,5 +1,7 @@
 use bevy::{prelude::*, render::render_resource::{Extent3d}, sprite::Rect};
 use crate::lemmings::*;
+use crate::lemmings::models::Animation;
+use crate::xbrz;
 
 pub struct LoadLemmingsTexturesPlugin;
 
@@ -28,46 +30,91 @@ fn u32_to_rgba_u8(u32s: &[u32]) -> Vec<u8> {
 // 4k is 10.8x high
 // Realistically: 6x then 2x to get 12: good enough for 4k.
 // Or should we do 5x then 2x to get 10 and have a little margin for 4k?
-fn scale_animation(a: &Animation) -> Animation {
-    let scale_a: usize = 6;
-    let scale_b: usize = 2;
-    let mut big_frames = Vec::<Vec<u32>>::new();
-    for frame in &a.frames {
-        let bigger = xbrz::scale(scale_a as u8, frame, a.width as u32, a.height as u32);
-        let biggest = xbrz::scale(scale_b as u8, &bigger, (a.width * scale_a) as u32, (a.height * scale_a) as u32);
-        big_frames.push(biggest);
-    }
-    Animation{
-        frames: big_frames,
-        width: a.width * scale_a * scale_b,
-        height: a.height * scale_a * scale_b,
-    }
+const SCALE: usize = 12; // Must be A*B.
+const SCALE_A: usize = 6;
+const SCALE_B: usize = 2;
+
+// Multi-step scale-up.
+fn multi_scale(image: &[u32], width: usize, height: usize) -> Vec<u32> {
+    let bigger = xbrz::scale(SCALE_A as u8, image, width as u32, height as u32);
+    xbrz::scale(SCALE_B as u8, &bigger, (width * SCALE_A) as u32, (height * SCALE_A) as u32)
 }
 
-fn load_lemmings_textures_startup(
-	mut commands: Commands,
-	mut images: ResMut<Assets<Image>>,
+// Figure out a neat way to layout the grid.
+fn cols_rows_for_frames(frame_count: usize) -> (usize, usize) {
+    let lenf = frame_count as f32;
+    let cols = lenf.sqrt().round() as usize;
+    let divides_perfectly = frame_count % cols == 0;
+    let rows = if divides_perfectly { frame_count / cols } else { frame_count / cols + 1};
+    (cols, rows)
+}
+
+fn make_atlas_from_animation(
+    animation: &Animation,
+    mut images: ResMut<Assets<Image>>,
 	mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let games = loader::load().unwrap();
-    let game = games.lemmings.unwrap();
-    let ml = &game.main.lemming_animations.mining_left;
-    let u8_data = u32_to_rgba_u8(&ml.frames[0]);
-    let image = Image::new(Extent3d{width: ml.width as u32, height: ml.height as u32, depth_or_array_layers: 1},
+) -> Handle<TextureAtlas> {
+    // TODO special case if only one treat as not an anim? Handle that in the parser?
+    let (cols, rows) = cols_rows_for_frames(animation.frames.len());
+    let scaled_width = animation.width * SCALE;
+    let scaled_height = animation.height * SCALE;
+    let atlas_width = (scaled_width + 1) * cols - 1; // 1px gap between each.
+    let atlas_height = (scaled_height + 1) * rows - 1;
+    let atlas_pixels = atlas_width * atlas_height;
+    let mut atlas = Vec::<u32>::with_capacity(atlas_pixels);
+    atlas.resize(atlas_pixels, 0);
+    let mut col: usize = 0;
+    let mut row: usize = 0;
+    let mut sprite_rects = Vec::<Rect>::with_capacity(animation.frames.len());
+    for small_frame in &animation.frames {
+        let scaled_frame = multi_scale(small_frame, animation.width, animation.height);
+        let start_atlas_x = col * (scaled_width + 1);
+        let mut atlas_y = row * (scaled_height + 1);
+
+        sprite_rects.push(Rect { 
+            min: Vec2 { x: start_atlas_x as f32, y: atlas_y as f32 },
+            max: Vec2 { x: (start_atlas_x + scaled_width) as f32, y: (atlas_y + scaled_height) as f32 } });
+
+        for frame_y in 0..scaled_height {
+            let mut atlas_x = start_atlas_x;
+            for frame_x in 0..scaled_width {
+                atlas[atlas_y * atlas_width + atlas_x] = scaled_frame[frame_y * scaled_width + frame_x];
+                atlas_x += 1;
+            }
+            atlas_y += 1;
+        }
+
+        // Move to the next slot.
+        col += 1;
+        if col >= cols {
+            col = 0;
+            row += 1;
+        }
+    }
+    // Convert it for bevy now.
+    let u8_data = u32_to_rgba_u8(&atlas);
+    let image = Image::new(Extent3d{width: atlas_width as u32, height: atlas_height as u32, depth_or_array_layers: 1},
         bevy::render::render_resource::TextureDimension::D2,
         u8_data,
         bevy::render::render_resource::TextureFormat::Rgba8Unorm);
     let image_handle = images.add(image);
-
-    let mut ta = TextureAtlas::new_empty(image_handle, Vec2::new(ml.width as f32, ml.height as f32));    
-    ta.add_texture(Rect {
-            min: Vec2::ZERO,
-            max: Vec2::new(ml.width as f32, ml.height as f32),
-        });
+    let mut ta = TextureAtlas::new_empty(image_handle, Vec2::new(atlas_width as f32, atlas_height as f32));    
+    for rect in sprite_rects {
+        ta.add_texture(rect);
+    }
     let ta_handle = texture_atlases.add(ta);
+    ta_handle
+}
 
+fn load_lemmings_textures_startup(
+	mut commands: Commands,
+	images: ResMut<Assets<Image>>,
+	texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let games = loader::load().unwrap();
+    let game = games.lemmings.unwrap();
 	let game_textures = GameTextures {
-		mining_right: ta_handle,
+		mining_right: make_atlas_from_animation(&game.main.lemming_animations.mining_right, images, texture_atlases),
 	};
 	commands.insert_resource(game_textures);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,46 @@ mod lemmings;
 mod lemmings_to_bevy;
 mod xbrz;
 
-use std::{fs, path};
+// use std::{fs, path};
 use bevy::{
     prelude::*,
     window::PresentMode,
 };
-use lemmings::models::Animation;
 use lemmings_to_bevy::load_lemmings_textures::GameTextures;
 
-use crate::lemmings::{loader, png};
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
+}
 
 fn setup(
     mut commands: Commands,
-    mut game_textures: Res<GameTextures>,
+    game_textures: Res<GameTextures>,
 ) {
     commands.spawn_bundle(Camera2dBundle::default());
     commands
         .spawn_bundle(SpriteSheetBundle {
             texture_atlas: game_textures.mining_right.clone(),
-            transform: Transform::from_scale(Vec3::splat(6.0)),
+            transform: Transform::from_scale(Vec3::splat(0.5)),
             ..default()
-        });
+        })
+        .insert(AnimationTimer(Timer::from_seconds(1. / 15., true)));
 }
 
 #[derive(Component)]
@@ -60,161 +79,7 @@ impl Plugin for HelloPlugin {
     }
 }
 
-struct MyAtlas {
-    data: Vec<u32>,
-    cols: usize,
-    rows: usize,
-    width: usize,
-    height: usize,
-}
-fn make_atlas_from_frames(frames: &Vec<Vec<u32>>, width: usize, height: usize) -> MyAtlas {
-    // Figure out the size to go for for the atlas.
-    // TODO special case if only one treat as not an anim? Handle that in the parser?
-    let len = frames.len();
-    let lenf = len as f32;
-    let cols = lenf.sqrt().round() as usize;
-    let divides_perfectly = len % cols == 0;
-    let rows = if divides_perfectly { len / cols } else { len / cols + 1};
-    let atlas_width = width * cols + (cols - 1); // 1px gap between each.
-    let atlas_height = height * rows + (rows - 1);
-    let mut atlas = Vec::<u32>::new();
-    atlas.resize(atlas_width * atlas_height, 0);
-    let mut col: usize = 0;
-    let mut row: usize = 0;
-    for frame in frames {
-        let start_atlas_x = col * (width + 1);
-        let mut atlas_y = row * (height + 1);
-        for frame_y in 0..height {
-            let mut atlas_x = start_atlas_x;
-            for frame_x in 0..width {
-                atlas[atlas_y * atlas_width + atlas_x] = frame[frame_y * width + frame_x];
-                atlas_x += 1;
-            }
-            atlas_y += 1;
-        }
-
-        // Move to the next slot.
-        col += 1;
-        if col >= cols {
-            col = 0;
-            row += 1;
-        }
-    }
-    MyAtlas{
-        data: atlas,
-        cols,
-        rows,
-        width: atlas_width,
-        height: atlas_height,
-    }
-}
-
-// 4k is 3840x2160
-// 5K is 5120x2880
-// Original game is 320x200
-// Since it scrolls horizontally, i only care about height for scaling.
-// 5k ratio is 14.4x high: could do 6x then 3x to get 18.
-// 4k is 10.8x high
-// Realistically: 6x then 2x to get 12: good enough for 4k.
-// Or should we do 5x then 2x to get 10 and have a little margin for 4k?
-fn scale_animation(a: &Animation) -> Animation {
-    let scale_a: usize = 6;
-    let scale_b: usize = 2;
-    let mut big_frames = Vec::<Vec<u32>>::new();
-    for frame in &a.frames {
-        let bigger = xbrz::scale(scale_a as u8, frame, a.width as u32, a.height as u32);
-        let biggest = xbrz::scale(scale_b as u8, &bigger, (a.width * scale_a) as u32, (a.height * scale_a) as u32);
-        big_frames.push(biggest);
-    }
-    Animation{
-        frames: big_frames,
-        width: a.width * scale_a * scale_b,
-        height: a.height * scale_a * scale_b,
-    }
-}
-
 fn main() {
-    // let games = loader::load().unwrap();
-    // let game = games.lemmings.unwrap();
-    // let rusty_path = format!("{}/rusty", game.path);
-    // fs::create_dir_all(&rusty_path).unwrap();
-    // for asset in game.all_assets() {
-    //     let filename_base = format!("{}/{}", rusty_path, asset.name);
-    //     match asset.content {
-    //         lemmings::models::AnimationOrImage::Animation(a) => {
-    //             {
-    //                 let atlas = make_atlas_from_frames(&a.frames, a.width, a.height);
-    //                 let filename = format!("{}.original.{}r.{}c.{}w.{}h.png", filename_base, atlas.cols, atlas.rows, a.width, a.height);
-    //                 if !path::Path::new(&filename).exists() {
-    //                     let png = png::png_data(atlas.width as u32, atlas.height as u32, &atlas.data);
-    //                     fs::write(filename, png).unwrap();
-    //                 }
-    //             }
-    //             {
-    //                 let scaled = scale_animation(&a);
-    //                 let atlas = make_atlas_from_frames(&scaled.frames, scaled.width, scaled.height);
-    //                 let filename = format!("{}.scaled.{}r.{}c.{}w.{}h.png", filename_base, atlas.cols, atlas.rows, scaled.width, scaled.height);
-    //                 if !path::Path::new(&filename).exists() {
-    //                     let png = png::png_data(atlas.width as u32, atlas.height as u32, &atlas.data);
-    //                     fs::write(filename, png).unwrap();
-    //                 }
-    //             }
-
-    //             // for (index, frame) in a.frames.iter().enumerate() {
-    //             //     {
-    //             //         let filename = format!("{}.original.{}.png", filename_base, index);
-    //             //         if !path::Path::new(&filename).exists() {
-    //             //             let png = png::png_data(a.width as u32, a.height as u32, &frame);
-    //             //             fs::write(filename, png).unwrap();
-    //             //         }
-    //             //     }
-    //             //     {
-    //             //         let filename = format!("{}.scaled.{}.png", filename_base, index);
-    //             //         if !path::Path::new(&filename).exists() {
-    //             //             let bigger = xbrz::scale(6, &frame, a.width as u32, a.height as u32);
-    //             //             let biggest = xbrz::scale(3, &bigger, (a.width * 6) as u32, (a.height * 6) as u32);
-    //             //             let png = png::png_data((a.width * 6 * 3) as u32, (a.height * 6 * 3) as u32, &biggest);
-    //             //             fs::write(filename, png).unwrap();
-    //             //         }
-    //             //     }
-    //             // }
-    //         },
-    //         lemmings::models::AnimationOrImage::Image(i) => {
-    //             {
-    //                 let filename = format!("{}.original.png", filename_base);
-    //                 if !path::Path::new(&filename).exists() {
-    //                     let png = png::png_data(i.width as u32, i.height as u32, &i.bitmap);
-    //                     fs::write(filename, png).unwrap();
-    //                 }
-    //             }
-    //             {
-    //                 let filename = format!("{}.scaled.png", filename_base);
-    //                 if !path::Path::new(&filename).exists() {
-    //                     let bigger = xbrz::scale(6, &i.bitmap, i.width as u32, i.height as u32);
-    //                     let biggest = xbrz::scale(3, &bigger, (i.width * 6) as u32, (i.height * 6) as u32);
-    //                     let png = png::png_data((i.width * 6 * 3) as u32, (i.height * 6 * 3) as u32, &biggest);
-    //                     fs::write(filename, png).unwrap();
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    // }
-    // // let bl = &game.main.lemming_animations.bashing_left;
-    // // for (index, frame) in bl.frames.iter().enumerate() {
-    // //     let filename = format!("{}/lemming_bashing_left_original_{}.png", rusty_path, index);
-    // //     if !path::Path::new(&filename).exists() {
-    // //     }
-
-    // //     let filename = format!("{}/lemming_bashing_left_scaled_{}.png", rusty_path, index);
-    // //     if !path::Path::new(&filename).exists() {
-    // //         let bigger = xbrz::scale(6, frame, bl.width as u32, bl.height as u32);
-    // //         let biggest = xbrz::scale(3, &bigger, (bl.width * 6) as u32, (bl.height * 6) as u32);
-    // //         let png = png::png_data((bl.width * 6 * 3) as u32, (bl.height * 6 * 3) as u32, &biggest);
-    // //         fs::write(filename, png).unwrap();
-    // //     }
-    // // }
-
     App::new()
         .insert_resource(WindowDescriptor {
             title: "Rusty Lemmings".to_string(),
@@ -224,9 +89,10 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
+        .add_startup_system(setup) // Post-normal stage?
         .add_plugin(HelloPlugin)
         .add_plugin(lemmings_to_bevy::load_lemmings_textures::LoadLemmingsTexturesPlugin)
+        .add_system(animate_sprite)
         .run();
 }
 
