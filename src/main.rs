c4556d6137496c22c83715d197915633bef2ff0e
Chris Hulbert <chris.hulbert@gmail.com>
2022-08-05 19:38:49 +1000

    Show menu

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ mod lemmings;
 mod lemmings_to_bevy;
 mod xbrz;
 
+use bevy::{
+    prelude::*,
+    window::PresentMode,
+};
+use lemmings_to_bevy::load_lemmings_textures::GameTextures;
+
 // Tested by watching frame-by-frame youtube captures.
 const FPS: f32 = 15.;
 const FRAME_DURATION: f32 = 1. / FPS;
@@ -24,14 +30,53 @@ const SCALE_B: usize = 2;
 const RES_W: usize = 1280;
 const RES_H: usize = 720;
 const ORIGINAL_GAME_H: usize = 200;
-const TEXTURE_SCALE: f32 = (RES_H as f32) / (ORIGINAL_GAME_H as f32) / (SCALE as f32);
+// I'm declaring an 'original game pixel' to be called a 'point'.
+const POINT_SIZE: f32 = (RES_H as f32) / (ORIGINAL_GAME_H as f32); // How many bevy transform values to get one 'point' (pixel) in the original game.
+const TEXTURE_SCALE: f32 = POINT_SIZE / (SCALE as f32);
 
-// use std::{fs, path};
-use bevy::{
-    prelude::*,
-    window::PresentMode,
-};
-use lemmings_to_bevy::load_lemmings_textures::GameTextures;
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum GameState {
+    MainMenu,
+}
+
+#[derive(Component)]
+struct BlinkAnimationTimer {
+    timer: Timer,
+    index: isize, // Set to negative if you want a delay before the blink.
+    dwell: isize, // Dwell time in frames after a blink.
+}
+
+fn animate_blinking_sprites(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut BlinkAnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in &mut query {
+        timer.timer.tick(time.delta());
+        if timer.timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            let texture_frames = texture_atlas.textures.len() as isize;
+
+            // Advance.
+            timer.index += 1;
+            let total_frames = texture_frames + timer.dwell;
+            if timer.index > total_frames {
+                timer.index = 0;
+            }
+
+            // Apply it. If it's in the initial delay or the dwell time, just show 0.
+            if 0 <= timer.index && timer.index < texture_frames {
+                sprite.index = timer.index as usize;
+            } else {
+                sprite.index = 0;
+            }
+        }
+    }
+}
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
@@ -54,6 +99,127 @@ fn animate_sprite(
     }
 }
 
+fn spawn_menu_logo(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+) {
+    let scale = Vec3::new(TEXTURE_SCALE / 2., TEXTURE_SCALE / 2., 1.); // Logo is svga, so halve everything.
+    const Y: f32 = 52.; // Make it overlap the background-tile-seam.
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: game_textures.logo.clone(),
+            transform: Transform{
+                translation: Vec3::new(0., Y * POINT_SIZE, 1.), 
+                scale,
+                ..default()
+            },        
+            ..default()
+        });
+
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: game_textures.blink1.clone(),
+            transform: Transform{
+                translation: Vec3::new(-138. * POINT_SIZE, (Y + 1.) * POINT_SIZE, 2.),
+                scale,
+                ..default()
+            },        
+            ..default()
+        })
+        .insert(BlinkAnimationTimer{
+            timer: Timer::from_seconds(FRAME_DURATION, true),
+            index: -15,
+            dwell: 30,
+        });
+
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: game_textures.blink2.clone(),
+            transform: Transform{
+                translation: Vec3::new(-26. * POINT_SIZE, (Y + 2.) * POINT_SIZE, 2.),
+                scale,
+                ..default()
+            },        
+            ..default()
+        })
+        .insert(BlinkAnimationTimer{
+            timer: Timer::from_seconds(FRAME_DURATION, true),
+            index: -22,
+            dwell: 45,
+        });
+
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: game_textures.blink3.clone(),
+            transform: Transform{
+                translation: Vec3::new(94. * POINT_SIZE, (Y + 5.5) * POINT_SIZE, 2.),
+                scale,
+                ..default()
+            },        
+            ..default()
+        })
+        .insert(BlinkAnimationTimer{
+            timer: Timer::from_seconds(FRAME_DURATION, true),
+            index: -30,
+            dwell: 37,
+        });
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: game_textures.blink3.clone(),
+            transform: Transform{
+                translation: Vec3::new(94. * POINT_SIZE, (Y + 5.5) * POINT_SIZE, 2.),
+                scale,
+                ..default()
+            },        
+            ..default()
+        })
+        .insert(BlinkAnimationTimer{
+            timer: Timer::from_seconds(FRAME_DURATION, true),
+            index: -30,
+            dwell: 37,
+        });
+
+        commands
+        .spawn_bundle(SpriteBundle {
+            texture: game_textures.mayhem.clone(),
+            transform: Transform{
+                translation: Vec3::new(0., 0., 2.),
+                scale,
+                ..default()
+            },        
+            ..default()
+        });
+}
+
+fn spawn_menu_background(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+) {
+    const BG_WIDTH: f32 = 320.; // Texture size in original game pixels (points).
+    const BG_HEIGHT: f32 = 104.;
+    fn spawn(commands: &mut Commands, game_textures: &Res<GameTextures>, x: f32, y: f32) {
+        commands
+        .spawn_bundle(SpriteBundle {
+            texture: game_textures.background.clone(),
+            transform: Transform{
+                translation: Vec3::new(x * POINT_SIZE, y * POINT_SIZE, 0.),
+                scale: Vec3::new(TEXTURE_SCALE, TEXTURE_SCALE, 1.),
+                ..default()
+            },        
+            ..default()
+        });
+    }
+    spawn(&mut commands, &game_textures, BG_WIDTH, BG_HEIGHT);
+    spawn(&mut commands, &game_textures, 0., BG_HEIGHT);
+    spawn(&mut commands, &game_textures, -BG_WIDTH, BG_HEIGHT);
+    spawn(&mut commands, &game_textures, BG_WIDTH, 0.);
+    spawn(&mut commands, &game_textures, 0., 0.);
+    spawn(&mut commands, &game_textures, -BG_WIDTH, 0.);
+    spawn(&mut commands, &game_textures, BG_WIDTH, -BG_HEIGHT);
+    spawn(&mut commands, &game_textures, 0., -BG_HEIGHT);
+    spawn(&mut commands, &game_textures, -BG_WIDTH, -BG_HEIGHT);
+}
+
 fn post_startup_setup(
     mut commands: Commands,
     game_textures: Res<GameTextures>,
@@ -63,7 +229,7 @@ fn post_startup_setup(
         .spawn_bundle(SpriteSheetBundle {
             texture_atlas: game_textures.mining_right.clone(),
             transform: Transform{
-                translation: Vec3::new(-100., 0., 0.),
+                translation: Vec3::new(0., 0., 1.),
                 scale: Vec3::new(TEXTURE_SCALE, TEXTURE_SCALE, 1.),
                 ..default()
             },        
@@ -74,7 +240,7 @@ fn post_startup_setup(
         .spawn_bundle(SpriteSheetBundle {
             texture_atlas: game_textures.blocking.clone(),
             transform: Transform{
-                translation: Vec3::new(100., 0., 0.),
+                translation: Vec3::new(100., 0., 1.),
                 scale: Vec3::new(TEXTURE_SCALE, TEXTURE_SCALE, 1.),
                 ..default()
             },     
@@ -117,7 +283,11 @@ impl Plugin for HelloPlugin {
 }
 
 fn main() {
+    // TODO think about how all the assets are centered, so that they can be blurry maybe?
+    // Especially seems to affect even numbered ones? Or odd?
     App::new()
+        .add_state(GameState::MainMenu)
+        .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
         .insert_resource(WindowDescriptor {
             title: "Rusty Lemmings".to_string(),
             width: RES_W as f32,
@@ -128,9 +298,12 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system_to_stage(StartupStage::PostStartup, post_startup_setup)
+        .add_startup_system_to_stage(StartupStage::PostStartup, spawn_menu_background)
+        .add_startup_system_to_stage(StartupStage::PostStartup, spawn_menu_logo)
         .add_plugin(HelloPlugin)
         .add_plugin(lemmings_to_bevy::load_lemmings_textures::LoadLemmingsTexturesPlugin)
         .add_system(animate_sprite)
+        .add_system(animate_blinking_sprites)        
         .run();
 }
 
