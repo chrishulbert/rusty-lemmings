973ffd7b1457d61462a97509c17f2913621bd2b4
Chris Hulbert <chris.hulbert@gmail.com>
2022-08-29 07:08:21 +1000

    Highlight, scale tweaks, padding

--- src/level_selection_menu.rs
+++ src/level_selection_menu.rs
@@ -34,18 +34,21 @@ impl Plugin for LevelSelectionMenuPlugin {
 fn button_highlight_system(
     windows: Res<Windows>,
     mouse_buttons: Res<Input<MouseButton>>,
-    mut buttons: Query<(&mut Sprite, &Transform), With<LevelSelectionButton>>,
+    mut buttons: Query<(&Transform, &Children), With<LevelSelectionButton>>,
+	mut letters: Query<&mut TextureAtlasSprite>,
 ) {
     if let Some(window) = windows.iter().next() {
         let position = window.cursor_position().unwrap_or(Vec2::NEG_ONE);
-        let x = position.x - window.width() / 2.;
         let y = position.y - window.height() / 2.;
-        for (mut sprite, transform) in &mut buttons {
-            let is_over = 
-                transform.translation.x - 120. <= x && x <= transform.translation.x + 120. &&
-                transform.translation.y - 61. <= y && y <= transform.translation.y + 61.;
+        for (transform, children) in &mut buttons {			
+            let is_over = transform.translation.y - 16. * transform.scale.y < y &&
+				y < transform.translation.y + 16. * transform.scale.y;
             let a: f32 = if is_over { if mouse_buttons.pressed(MouseButton::Left) { 0.5 } else { 0.8 } } else { 1. };
-            sprite.color.set_a(a);
+			for &child in children {
+				if let Ok(mut letter) = letters.get_mut(child) {
+					letter.color.set_a(a);
+				}
+			}
         }
     }
 }
@@ -71,13 +74,13 @@ fn spawn_background(
 		});
 }
 
-fn text_size(scale: f32) -> f32 {
-	16.0 * scale * POINT_SIZE / 2.
+fn text_size() -> f32 {
+	16.0 * POINT_SIZE / 2.
 }
 
-fn spawn_text(text: &str, parent: &mut ChildBuilder, game_textures: &Res<GameTextures>, scale_factor: f32, y: f32) {
-	let texture_scale = scale_factor * TEXTURE_SCALE / 2.; // Logo is SVGA so halve it.
-	let size = text_size(scale_factor);
+fn spawn_text(text: &str, parent: &mut ChildBuilder, game_textures: &Res<GameTextures>) {
+	let texture_scale = TEXTURE_SCALE / 2.; // Logo is SVGA so halve it.
+	let size = text_size();
 	let scale = Vec3::new(texture_scale, texture_scale, 1.);
 	let mut x: f32 = -(text.len() as f32) / 2. * size;
 	for c in text.chars() {
@@ -89,7 +92,7 @@ fn spawn_text(text: &str, parent: &mut ChildBuilder, game_textures: &Res<GameTex
 				sprite: TextureAtlasSprite{index, ..default()},
 				transform: Transform {
 					scale,
-					translation: Vec3::new(x, y, 2.),
+					translation: Vec3::new(x, 0., 3.),
 					..default()
 				},        
 				..default()
@@ -108,13 +111,18 @@ pub struct LevelSelectionButton{
 
 fn spawn_level_button(parent: &mut ChildBuilder, game_textures: &Res<GameTextures>, name: &str, scale: f32, y: f32, game_id: &str, skill: isize) {
 	parent.spawn_bundle(SpatialBundle{
+		transform: Transform {
+			translation: Vec3::new(0., y, 2.),
+			scale: Vec3::new(scale, scale, 1.),
+			..default()
+		},        
 		..default()
 	}).insert(LevelSelectionButton{
 		game_id: game_id.to_owned(),
 		skill,
 		level_name: name.to_owned(),		
 	}).with_children(|parent| {
-		spawn_text(name, parent, game_textures, scale, y);
+		spawn_text(name, parent, game_textures);
 	});
 }
 
@@ -130,11 +138,12 @@ fn spawn_levels(
 		.with_children(|parent| {
 			let names = names_per_game_and_skill(&game_selection.0, skill_selection.0);
 			let scale: f32 = if names.len() >= 16 { 0.5 } else { 1. };
-			let size = text_size(scale);
-			let mut y: f32 = -(names.len() as f32) / 2. * size;
+			let padding: f32 = POINT_SIZE * 4. * scale;
+			let size = text_size() * scale;
+			let mut y: f32 = -((names.len() as f32) * (size + padding) - padding) / 2.;
 			for name in names {
 				spawn_level_button(parent, &game_textures, &name, scale, y, &game_selection.0, skill_selection.0);
-				y += size
+				y += size + padding;
 			}
 		});
 }
