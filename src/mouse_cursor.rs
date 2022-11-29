009f0fde8c6faa728d861285b89ac141f274c289
Chris Hulbert <chris.hulbert@gmail.com>
2022-11-29 19:35:05 +1100

    Show cursor

--- src/mouse_cursor.rs
+++ src/mouse_cursor.rs
@@ -3,6 +3,8 @@ use bevy::input::mouse::MouseMotion;
 use crate::{POINT_SIZE, TEXTURE_SCALE};
 use crate::{GameTextures};
 
+const MOUSE_Y: f32 = 999.; // On top of all.
+
 pub struct MouseCursorPlugin;
 
 impl Plugin for MouseCursorPlugin {
@@ -22,7 +24,7 @@ fn spawn_mouse_cursor(
     commands.spawn_bundle(SpriteBundle {
         texture: game_textures.mouse_cursor.clone(),
         transform: Transform{
-            translation: Vec3::new(0., 0., 999.),
+            translation: Vec3::new(99999., 99999., MOUSE_Y),
             scale: Vec3::new(TEXTURE_SCALE, TEXTURE_SCALE, 1.),
             ..default()
         },        
@@ -34,17 +36,22 @@ fn spawn_mouse_cursor(
 fn mouse_motion_system(
     windows: Res<Windows>,
     mut mouse_motion_events: EventReader<MouseMotion>,
+    mut mouse_cursor_component_query: Query<&mut Transform, With<MouseCursorComponent>>,
 ) {
-    // Is there some motion?
-    let Some(event) = mouse_motion_events.iter().next() else { return };
+    // Is there some motion? Otherwise dont bother with the remainder, as an optimisation.
+    let Some(_) = mouse_motion_events.iter().next() else { return };
 
     // Find the window.
     let Some(window) = windows.iter().next() else { return };
 
-    if let Some(position) = window.cursor_position() {
-        // Move and show it.
-        // todo
-    } else {
-        // Off-window, so hide it.
+    for (mut transform) in &mut mouse_cursor_component_query {
+        if let Some(position) = window.cursor_position() {
+            
+            // Move and show it.
+            transform.translation = Vec3::new(position.x - window.width() / 2., position.y - window.height() / 2., MOUSE_Y);
+        } else {
+            // Off-window, so hide it.
+            transform.translation = Vec3::new(99999., 99999., MOUSE_Y);
+        }
     }
 }
